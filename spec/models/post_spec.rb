38b765b93e5ec7c122ec0bc0a47fcaa70bce9ecc
Kyle Harmon <KyleHarmon52@gmail.com>
2025-06-09 11:25:34 -0400

    Add association spec for Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "belongs to a user" do
+    assoc = described_class.reflect_on_association(:user)
+    expect(assoc.macro).to eq(:belongs_to)
+  end
 end
